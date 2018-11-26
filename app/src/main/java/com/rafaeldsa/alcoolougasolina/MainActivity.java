8aeaf377c0017c2fc88566af87880a16a4af4c55
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2018-11-25 21:31:11 -0300

    Finished project

---
+++ app/src/main/java/com/rafaeldsa/alcoolougasolina/MainActivity.java
@@ -0,0 +1,57 @@
+package com.rafaeldsa.alcoolougasolina;
+
+
+import android.content.Context;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Usuario user;
+    Vibrator v;
+    EditText preco;
+    double valorResultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        user = new Usuario();
+
+        preco = (EditText)findViewById(R.id.preco);
+        final Button btn = (Button)findViewById(R.id.btn);
+        final TextView txtResultado = (TextView)findViewById(R.id.txtResultado);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                valorResultado = (Double.parseDouble(preco.getText().toString()) * 0.7);
+                String texto = "Abasteça com Álcool se ele custar até: R$ ";
+                texto += formataValor(valorResultado);
+                txtResultado.setText(texto);
+
+                v = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(500);
+            }
+        });
+
+    }
+    private void metodosUsuario(){
+        user.setPrecoo(preco.getText().toString());
+    }
+
+    private String formataValor(double valor) {
+        return String.format(Locale.FRANCE,"%.2f",valor);
+    }
+
+
+}
+
---
+++ app/src/main/java/com/rafaeldsa/alcoolougasolina/Usuario.java
@@ -0,0 +1,14 @@
+package com.rafaeldsa.alcoolougasolina;
+
+public class Usuario {
+    private String precoo;
+
+
+    public String getPrecoo() {
+        return precoo;
+    }
+
+    public void setPrecoo(String precoo) {
+        this.precoo = precoo;
+    }
+}
